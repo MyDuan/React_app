68608686b0e68f62762c4e408c2042bee79da10b
MyDuan <dy1226130575@163.com>
2019-06-17 00:00:49 +0900

    Updates

--- precache-manifest.8aae068f7d69abc9e6e8b83fe1dd5d23.js
+++ precache-manifest.8aae068f7d69abc9e6e8b83fe1dd5d23.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3a7051caf9df4cd3aa65306b9f708548",
+    "revision": "1ac96db7080b2128da6063e217bccb78",
     "url": "/index.html"
   },
   {
-    "revision": "4b623e8593c9ac990379",
+    "revision": "fa16cdedc8745032a3c3",
     "url": "/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "2e0129063922e3bb197a",
-    "url": "/static/js/2.b41502e9.chunk.js"
+    "revision": "94f4f668688e3ef688c3",
+    "url": "/static/js/2.b61bcfdd.chunk.js"
   },
   {
-    "revision": "4b623e8593c9ac990379",
-    "url": "/static/js/main.28647029.chunk.js"
+    "revision": "fa16cdedc8745032a3c3",
+    "url": "/static/js/main.40e471e3.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.8aae068f7d69abc9e6e8b83fe1dd5d23.js"
 );
 
 self.addEventListener('message', (event) => {
