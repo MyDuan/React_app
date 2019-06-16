8a3f8615cfe7ef1286d3bbfe7c834cd2cd95f3cd
MyDuan <dy1226130575@163.com>
2019-06-17 08:57:57 +0900

    CHange some small parts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Create A sample React App.
         </a>
       </header>
     </div>
