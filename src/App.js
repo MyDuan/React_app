e3087f936d35a8eae576cef436968c4d6745086f
MyDuan <dy1226130575@163.com>
2019-06-17 09:43:50 +0900

    Use component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Create A sample React App.
-        </a>
-      </header>
-      <div>
-      <Route path='/'/>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Create A sample React App.
+          </a>
+        </header>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
